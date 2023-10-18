91f70a969cf42b3f5b1dc3ebc710ecbc06348144
Badrul <Badrul@host.docker.internal>
2023-10-18 17:08:50 +0800

    Save the new version

---
+++ Exercise2023/src/Week01/L01Q02.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Week01;
+
+/**
+ *
+ * @author Badrul
+ */
+public class L01Q02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=0;i<10;i++)
+            System.out.println("Hello World " + i);
+    }
+    
+}
