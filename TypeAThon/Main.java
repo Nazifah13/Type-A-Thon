fc85e57c4b87b5d7a51a8fd73f8fac185488986e
Bbulat7804 <muhdhazimnidzam@gmail.com>
2025-06-19 01:48:27 +0800

    Update Main.java

--- TypeAThon/Main.java
+++ TypeAThon/Main.java
@@ -14,6 +14,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("saya sayang nazifah ");
+        while(true){
+            System.out.println("sangat ");
+        }
     }
     
 }
